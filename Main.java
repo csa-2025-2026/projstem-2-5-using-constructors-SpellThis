3b236b9361262e27b6597043db175b0188730034
Ben_T <bmtran102@student.hbuhsd.edu>
2025-10-02 17:42:21 +0000

    hellom

--- Main.java
+++ Main.java
@@ -4,6 +4,38 @@ public class Main
 {
   public static void main(String[] args)
   {
+    Circle c1 = new Circle(10.1);// makes a circle radius 10.1
+    Circle c2 = new Circle(14.0);
+    Circle c3 = new Circle(20.5);
+
+    System.out.println(c1.toString());
+    String output = c2.toString();
+    System.out.println(output);
+    System.out.printf("%s%n", c3.toString());
     // write solutions to problems here
+  }
+
+  public static void main2(String[] args)
+  {
+   double length = 2.1;
+   double width = 31.2
+    System.out.println("Type a number for length and width:" + Rectangle(length, width));
+`   Scanner in = new Scanner(System.in);
+   length = in.nextDouble();
+   width = in.nextDouble();
+   Rectangle r1 = new Rectangle(length, width);
+   Rectangle r2 = new Rectangle(5.0);
+   Rectangle r3 = new Rectangle();
+
+   System.out.println(r1.toString());
+   String output = r2.toString();
+   System.out.println(output);
+   System.out.printf("%s%n", r3.toString());
+
+  }
+
+  public RegularPolygon(int num,double len);
+  {
+System.out.println( )
   }
 }
